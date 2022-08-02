6c0a873f39fbebf66d82cd4e5436f476371078df
Annie-Talbot <a.c.t.july@gmail.com>
2022-08-02 22:17:41 +0100

    Instructors display on home -only add-able through admin at the moment

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,20 @@
+from enum import Enum
+
 from django.db import models
 
 
+class Experience:
+    levels = [
+        (0, "No Experience"),
+        (1, "Little Experience"),
+        (2, "Competent"),
+        (3, "Very Competent"),
+    ]
+
+
+class DingyInstructor(models.Model):
+    name = models.CharField(max_length=127, unique=True)
+    experience = models.IntegerField(choices=Experience.levels, default=0)
+
+    def __str__(self):
+        return self.name
